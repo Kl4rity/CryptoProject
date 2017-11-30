43ce59422d35a8a1c591c00cf264e53f1ba1c38b
rappai13 <rappai13@users.noreply.github.com>
2017-11-30 11:58:12 +0100

    Included Avgfunction

--- ethereum.js
+++ ethereum.js
@@ -1,8 +1,25 @@
-var rawdata = [];
-var blocks = new XMLHttpRequest();
-    blocks.open("GET", "https://etherchain.org/api/txs/0/5", false);
-    blocks.send();
 
-rawdata.push(blocks.responseText);
+var oBlocks = new XMLHttpRequest();
+    oBlocks.open("GET", "https://etherchain.org/api/blocks/0/2", false);
+    oBlocks.send();
 
-console.log(rawdata.toString());
+
+var oData = JSON.parse(oBlocks.responseText);
+
+var fAverageFee = function(){
+  var nTxcount = 0;
+  nTxcount = nTxcount + oData.data.forEach(function(block){
+    console.log(block.tx_count);
+    return block.tx_count;
+  });
+  var nTxfees = 0;
+  nTxfees = nTxfees + oData.data.forEach(function(block){
+    console.log(block.totalFee);
+    return block.totalFee;
+  });
+  return nTxfees;
+  return nTxcount;
+}
+console.log(oBlocks);
+console.log(oData);
+console.log(fAverageFee());
